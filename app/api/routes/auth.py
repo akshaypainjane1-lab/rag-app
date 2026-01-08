0df1a65ded8170c6897e71297b0bc206ea0b9752
Painjaneakshay11 <akshaypainjane11@gmail.com>
2026-01-08 17:38:55 +0530

    a

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
